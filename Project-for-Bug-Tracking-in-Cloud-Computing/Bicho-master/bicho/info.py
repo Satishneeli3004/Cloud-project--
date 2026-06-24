7f9f05ab5f594dbc671581052522a413ebedd04f
Satishneeli3004 <80031525+Satishneeli3004@users.noreply.github.com>
2026-06-24 22:52:30 +0530

    Delete Project-for-Bug-Tracking-in-Cloud-Computing/Bicho-master directory

--- Project-for-Bug-Tracking-in-Cloud-Computing/Bicho-master/bicho/info.py
+++
@@ -1,7 +0,0 @@
-PACKAGE = "bicho"
-VERSION = "0.9"
-
-AUTHOR = "GSyC/LibreSoft, Universidad Rey Juan Carlos"
-AUTHOR_EMAIL = "metrics-grimoire@lists.libresoft.es"
-COPYRIGHT = "Copyright (C) 2007-2013 GSyC/LibreSoft, Universidad Rey Juan Carlos"
-DESCRIPTION = "An analysis tool for issue tracking systems"
